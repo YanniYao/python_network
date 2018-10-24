bb03a82733e4d2f742550fae8ffd82362430e934
Yao <mariayao1018@gmail.com>
2018-10-24 09:24:07 +0800

    初步增加了文件压缩传输功能，但是存在中文文件名编解码问题；初步修改了协议格式，还需完善；需要将现在的全局变量配合多线程进行修改代码；提取特定功能块

--- client.py
+++ client.py
@@ -4,26 +4,31 @@ import filetype #用于判断非txt类型的其他文件类型
 import struct
 import os
 import sys
+import gzip
 
 HOST = '127.0.0.1'
 PORT = 50000
 Addr = (HOST, PORT)
-ID = '001'
 MAX_SIG_NUM = 50
+HEAD_FORMAT = "!3sI" #定义文件信息大小，!表示我们要使用网络字节序顺序解析，s前面的数字表示几个字节长的字符串，I表示integer或long
+HEAD_SIZE = struct.calcsize(HEAD_FORMAT)
+Iscompress = 'c'
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect(Addr)
 
-def recvFile(filename):
-	filename_utf8 = filename.encode(encoding='utf-8')
+ID = socket.gethostname()
+
+def recvFile(filename, is_compress):
+	filename_utf8 = (filename + is_compress).encode(encoding='utf-8')
 	clientSocket.send(filename_utf8)
 	while True:
 		if "has_file" == (clientSocket.recv(4096)).decode("utf-8"):
 			print("start receiving file......")
-			fileinfo_size = struct.calcsize('!5s128sI')
+			fileinfo_size = struct.calcsize('!20s128sI')
 			cmd_head = clientSocket.recv(fileinfo_size)
 			if cmd_head:
-				fileType, filename, filesize = struct.unpack('!5s128sI', cmd_head)
+				fileType, filename, filesize = struct.unpack('!20s128sI', cmd_head)
 				print("from server: [fileType]:", fileType.decode('utf-8') + ",[filename]:", (filename.decode('utf-8')).strip('\00') + ",[filesize]:", filesize)
 				fn = (filename.decode('utf-8')).strip('\00')
 				filelocalname = os.path.join('./Desktop/', 'new_' + fn)
@@ -45,14 +50,6 @@ def recvFile(filename):
 					sys.stdout.write(r)
 					sys.stdout.flush()
 				sys.stdout.write("\n")
-				'''
-				while recvd_size < filesize:
-					print("11111111")
-					recv_data = clientSocket.recv(1024)
-					data += recv_data
-					recvd_size += len(recv_data)
-				fp.write(data)
-				'''
 				print("received successfully!")
 			fp.close()
 		elif "no_such_file" == (clientSocket.recv(4096)).decode("utf-8"):
@@ -61,82 +58,98 @@ def recvFile(filename):
 			print("Invalid message from server!")
 		break
 
-def sendFile(filename):
-	if os.path.isfile(filename):
+def sendFile(cmd, filename):
+	print("filename:", filename)
+	if not os.path.isfile(filename):
+		print("The file is not exist!")
+		return
+	else:
+		info = (filename + "|" + Iscompress).encode()
+		cmd_size = len(info)
+		print("cmd_size:", cmd_size)
+		cmd_head = struct.pack(HEAD_FORMAT, cmd.encode(), cmd_size) + info
+		clientSocket.send(cmd_head)
+	buf = clientSocket.recv(HEAD_SIZE)
+	rpl, rest_pkt_size = struct.unpack(HEAD_FORMAT, buf)
+	info = clientSocket.recv(rest_pkt_size)
+	rpl = rpl.decode()
+	info = info.decode()
+	if 'PUT' == rpl and 'OK' == info:
 		if filename.split(".")[-1] == 'txt':
 			fileType = filename.split(".")[-1]
 		else:
 			fileType = (filetype.guess(filename)).extension
-		#定义文件信息大小，!表示我们要使用网络字节序顺序解析，s前面的数字表示几个字节长的字符串，I表示integer或long
-		fileinfo_size = struct.calcsize('!3s5s128sI')
-		cmd_clientID = ID.encode('utf-8')
-		cmd_fileType = fileType.encode('utf-8')
-		cmd_filename = os.path.basename(filename).encode('utf-8')
-		cmd_filesize = os.stat(filename).st_size
-		cmd_head = struct.pack('!3s5s128sI', cmd_clientID, cmd_fileType, cmd_filename, cmd_filesize)
-		clientSocket.send(cmd_head)
+		file_base_name = os.path.basename(filename)
+		if Iscompress == 'c':
+			fp = open(filename, 'rb')
+			compress_data = gzip.compress(fp.read(), compresslevel=9)
+			filesize = len(compress_data)
+		elif Iscompress == 'o':
+			filesize = os.stat(filename).st_size
+		else:
+			print("Whether the file is compressed is unknown...")
+		cmd_filesize = str(filesize)
+		cmd_info = ID + "|" + fileType + "|" + file_base_name + "|" + cmd_filesize
+		msg_len = len(cmd_info)
+		msg_head = struct.pack(HEAD_FORMAT, 'MSG'.encode(), msg_len)
+		clientSocket.send(msg_head + cmd_info.encode())
 		print("start sending file......")
-		data_send = 0
-		send_filesize = cmd_filesize
-		with open(filename, 'rb') as fp:
-			while send_filesize:
-				content = fp.read(4096)
-				clientSocket.send(content)
-				data_send += len(content)
-				send_filesize -= len(content)
-				rate = data_send / cmd_filesize
-				rate_num = int(rate * 100)
-				sig_num = int(MAX_SIG_NUM * rate)
-				r = '\r[%-50s]%d%%' % ('#' * sig_num, rate_num, )
-				sys.stdout.write(r)
-				sys.stdout.flush()
-			sys.stdout.write("\n")
-		print("send file successfully!")
+		if Iscompress == 'c':
+			clientSocket.sendall(compress_data)
+		else:
+			data_send = 0
+			send_filesize = filesize
+			with open(filename, 'rb') as fp:
+				while send_filesize:
+					content = fp.read(4096)
+					clientSocket.send(content)
+					data_send += len(content)
+					send_filesize -= len(content)
+					rate = data_send / filesize
+					rate_num = int(rate * 100)
+					sig_num = int(MAX_SIG_NUM * rate)
+					r = '\r[%-50s]%d%%' % ('#' * sig_num, rate_num, )
+					sys.stdout.write(r)
+					sys.stdout.flush()
+				sys.stdout.write("\n")
 		fp.close()
 	else:
 		print("The file is not exist!")
 
-def confirm(confirm_command):
-	confirm_command_utf8 = confirm_command.encode(encoding='utf-8')
-	clientSocket.sendall(confirm_command_utf8)
-	msg_utf8 = clientSocket.recv(4096)
-	msg = msg_utf8.decode()
-	print("receive message : ", msg)
-	if msg == 'no problem':
-		return True
-	else:
-		return False
-
-def operation1(filename):
+def operation1(filename, is_compress):
 	if confirm('get'):
-		recvFile(filename)
+		recvFile(filename, is_compress)
 	else:
 		print("server error!")
 
-def operation2(filename):
-	if confirm('put'):
-		sendFile(filename)
-	else:
-		print("server error!")
 
 try:
     while True:
         command = input(">>>")
         if not command:
             continue
-        elif command == 'exit':
-            command_utf8 = command.encode(encoding='utf-8')
-            clientSocket.sendall(command_utf8)
+        elif command == 'END':
+            cmd_head = struct.pack(HEAD_SIZE, command.encode(), "END".encode())
+            clientSocket.send(cmd_head)
             print("The connection is closed!")
             clientSocket.close()
             break
-        msg = command.split()
-        if len(msg) == 2 and msg[0] == 'get':
-    	    operation1(msg[1])
-        elif len(msg) == 2 and msg[0] == 'put':
-    	    operation2(msg[1])
         else:
-    	    print("Wrong command!")
+        	msg = command.split()
+        	if len(msg) == 2 and msg[0] == 'GET':
+        		is_compress = input("please choose the file transfer mode (o or c only (o for original) (c for compress)):")
+        		if not is_compress == 'o' and not is_compress == 'c' and not is_compress:
+        			continue
+        		else:
+        			recvFile(msg[1], is_compress)
+        	elif len(msg) == 2 and msg[0] == 'PUT':
+        		Iscompress = input("please choose the file transfer mode (o or c only (o for original) (c for compress)):")
+        		if not Iscompress == 'o' and not Iscompress == 'c' and not Iscompress:
+        			continue
+        		else:
+        			sendFile(msg[0], msg[1])
+        	else:
+        		print("Wrong command!")
 except socket.error as e:
 	print("error : ", e)
 	print("An error ocurred!")
--- server.py
+++ server.py
@@ -7,12 +7,16 @@ import time
 import signal
 import filetype
 import threading
+import gzip
 
 HOST = '127.0.0.1'
 PORT = 50000
 ThreadNum = 0
 Addr = (HOST, PORT)
 LOG_FOPMAT = "[{TIME}] {OUT}"
+HEAD_FORMAT = "!3sI"
+HEAD_SIZE = struct.calcsize(HEAD_FORMAT)
+Iscompress = 'c'
 terminate = False
 kill_str = {
 	'linux': "kill -9 {PID}",
@@ -32,26 +36,33 @@ def log(*args):
     output = ''.join(args).replace('\n', ' ')
     print(LOG_FOPMAT.format(TIME=cur_time, OUT=output))
 
-def recvFile(clientSocket):
-	msg = 'no problem'
-	msg_utf8 = msg.encode("utf-8")
-	clientSocket.send(msg_utf8)
-	fileinfo_size = struct.calcsize('!3s5s128sI')
-	cmd_head = clientSocket.recv(fileinfo_size)
+def recvFile(clientSocket, info):
+	Iscompress = info.split('|')[1]
+	buf = clientSocket.recv(HEAD_SIZE)
+	cmd, rest_pkt_size = struct.unpack(HEAD_FORMAT, buf)
+	info = clientSocket.recv(rest_pkt_size)
+	cmd = cmd.decode()
+	info = info.decode()
 	while True:
-	    if cmd_head:
-	    	clientID, fileType, filename, filesize = struct.unpack('!3s5s128sI', cmd_head)
-	    	fn = (filename.decode('utf-8')).strip('\00')
-	    	filelocalname = os.path.join('./Desktop/', 'new_' + fn)
+	    if 'MSG' == cmd:
+	    	clientID, fileType, filename, filesize = info.split('|')
+	    	if Iscompress == 'c':
+	    		filelocalname = os.path.join('./Desktop/', 'compress_' + filename)
+	    	elif Iscompress == 'o':
+	    		filelocalname = os.path.join('./Desktop/', 'new_' + filename)
+	    	else:
+	    		log("Whether the file is compressed is conflicting...")
+	    	fp = open(filelocalname, 'wb')
+	    	print("from client ", clientID + ":[fileType]:", fileType + ",[filename]:", filename + ",[filesize]:", filesize)
+	    	log("start receiving file......")
 	    	recvd_size = 0 #已接收文件大小
 	    	data = b''
-	    	fp = open(filelocalname, 'wb')
-	    	print("from client ", clientID.decode('utf-8')  + ":[fileType]:", fileType.decode('utf-8') + ",[filename]:", (filename.decode('utf-8')).strip('\00') + ",[filesize]:", filesize)
-	    	log("start receiving file......")
-	    	while recvd_size < filesize:
+	    	while recvd_size < int(filesize):
 	    		recv_data = clientSocket.recv(1024)
 	    		data += recv_data
 	    		recvd_size += len(recv_data)
+	    	if Iscompress == 'c':
+	    		data = gzip.decompress(data)
 	    	fp.write(data)
 	    	log("received successfully!")
 	    fp.close()
@@ -73,15 +84,6 @@ def sendFile(clientSocket):
 		cmd_head = struct.pack('!5s128sI', fileType.encode('utf-8'), os.path.basename(filename).encode('utf-8'), os.stat(filename).st_size)
 		clientSocket.send(cmd_head)
 		log("start sending file......")
-		'''
-		fp = open(filename, 'rb')
-		while True:
-			data = fp.read(1024)
-			if not data:
-				break
-			clientSocket.sendall(data)
-			break
-		'''
 		cmd_filesize = os.stat(filename).st_size
 		send_filesize = cmd_filesize
 		with open(filename, 'rb') as fp:
@@ -95,18 +97,30 @@ def sendFile(clientSocket):
 		clientSocket.send(NO_SUCH_FILE.encode("utf-8"))
 
 def acc_link(clientSocket, addr):
+	rpl = None
+	rest = None
 	while True:
-		msg_utf8 = clientSocket.recv(4096)
-		msg = msg_utf8.decode()
-		if msg == 'exit':
+		buf = clientSocket.recv(HEAD_SIZE)
+		cmd, rest_pkt_size = struct.unpack(HEAD_FORMAT, buf)
+		info = clientSocket.recv(rest_pkt_size)
+		print("cmd is:", type(cmd))
+		cmd = cmd.decode()
+		info = info.decode(errors='ignore')
+		if cmd == 'END':
 			log("The client requests to close the connection.")
 			clientSocket.close()
 			log("The connection from %s:%s closed." % addr)
 			break
-		elif msg == 'get':
+		elif cmd == 'GET':
 			sendFile(clientSocket)
-		elif msg == 'put':
-			recvFile(clientSocket)
+		elif cmd == 'PUT':
+			rpl = 'PUT'
+			status = 'OK'
+			confirm = status.encode()
+			rest_pkt_size = len(confirm)
+			reply = struct.pack(HEAD_FORMAT, rpl.encode(), rest_pkt_size)
+			clientSocket.send(reply + confirm)
+			recvFile(clientSocket, info)
 		else:
 			print("client error!")
 			clientSocket.close()
